68147677dc61de1d175aeb3857a47bc71ad43740
gael gohoungo <gohoungogael@gmail.com>
2021-01-21 10:22:18 -0500

    created conted context and reducer

--- src/components/incomeExpenses.js
+++ src/components/incomeExpenses.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
-export const incomeExpenses = () => {
-    return (
-        <div>
-            <div className="inc-exp-container">
+//Money formatter function
+function moneyFormatter(num) {
+  let p = num.toFixed(2).split('.');
+  return (
+    '$ ' +
+    p[0]
+      .split('')
+      .reverse()
+      .reduce(function (acc, num, i, orig) {
+        return num === '-' ? acc : num + (i && !(i % 3) ? ',' : '') + acc;
+      }, '') +
+    '.' +
+    p[1]
+  );
+}
+
+export const IncomeExpenses = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  const amounts = transactions.map(transaction => transaction.amount);
+
+  const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0);
+
+  const expense = (
+    amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  );
+
+  return (
+    <div className="inc-exp-container">
         <div>
           <h4>Income</h4>
   <p className="money plus">{moneyFormatter(income)}</p>
@@ -13,6 +42,5 @@ export const incomeExpenses = () => {
   <p className="money minus">{moneyFormatter(expense)}</p>
         </div>
       </div>
-        </div>
-    )
+  )
 }
