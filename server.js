3bdbd120aba795f6abbe589f06abf86238523e48
gael gohoungo <gohoungogael@gmail.com>
2021-01-25 10:40:44 -0500

    Modified Server

--- server.js
+++ server.js
@@ -29,4 +29,4 @@ if(process.env.NODE_ENV === 'production') {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on Port ${PORT}`.yellow.bold));
